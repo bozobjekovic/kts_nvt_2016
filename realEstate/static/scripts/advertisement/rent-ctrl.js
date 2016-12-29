d6561d5e0cde5088e6121f13a341caeaab796136
majamiljic <majamiljic2@gmail.com>
2016-12-30 00:57:32 +0100

    #61 Fixed submenus. Fixed filtering for menus. Fixed collapse expand panels. TODO: Filtering

--- realEstate/static/scripts/advertisement/rent-ctrl.js
+++ realEstate/static/scripts/advertisement/rent-ctrl.js
@@ -10,6 +10,22 @@
 			RentFactory.getAdvertisements().then(function(items) {
 			      $scope.rentAdvertisements = items;
 			   });
+			
+			RentFactory.getLocations().then(function(items) {
+			      $scope.locations = items;
+			});
+			
+			$scope.getAdvertisementsByType = function(purpose, category, type) {
+				RentFactory.getAdvertisementsByType(purpose, category, type).then(function(items){
+					$scope.rentAdvertisements = items;
+				});
+			};
+			
+			$scope.getAdvertisementsByCategory = function(purpose, category) {
+				RentFactory.getAdvertisementsByCategory(purpose, category).then(function(items){
+					$scope.rentAdvertisements = items;
+				});
+			};
 
 			$scope.showResidentalMenu = function() {
 				$scope.menu = 'residental';
--- realEstate/static/scripts/advertisement/rent-factory.js
+++ realEstate/static/scripts/advertisement/rent-factory.js
@@ -1,12 +1,34 @@
 angular.module('realEstateClientApp')
 	.factory('RentFactory', ['Restangular', '_', function(Restangular, _) {
 		'use strict';
-		
+
 		var advertisements = [];
+		var locations = [];
 		var retVal = {};
 		
 		retVal.getAdvertisements = function() {
 			return Restangular.all("advertisments/purpose/rent").getList().then(function(entries) {
+				advertisements = entries;
+				return advertisements;
+    		});
+		};
+		
+		retVal.getLocations = function() {
+			return Restangular.all("locations/all").getList().then(function(entries) {
+				locations = entries;
+				return locations;
+    		});
+		};
+
+		retVal.getAdvertisementsByType = function(purpose, category, type) {
+			return Restangular.one("advertisments/purpose", purpose).one("category", category).one("type", type).getList().then(function(entries) {
+				advertisements = entries;
+				return advertisements;
+    		});
+		};
+		
+		retVal.getAdvertisementsByCategory = function(purpose, category) {
+			return Restangular.one("advertisments/purpose", purpose).one("category", category).getList().then(function(entries) {
 				advertisements = entries;
 				return advertisements;
     		});
