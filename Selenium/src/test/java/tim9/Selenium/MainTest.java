6878dc647ef7a94fda104ac75277f2ff25f4679c
Ana Marojevic <ana94ns@hotmail.com>
2017-01-14 19:17:20 +0100

    Selenium setup, closed #95

---
+++ Selenium/src/test/java/tim9/Selenium/MainTest.java
@@ -0,0 +1,29 @@
+package tim9.Selenium;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class MainTest {
+	
+	private WebDriver browser;
+	
+	@BeforeMethod
+	public void setupSelenium() {
+		System.setProperty("webdriver.chrome.driver", "D:/Documents/Downloads/chromedriver_win32/chromedriver.exe");
+		browser = new ChromeDriver();
+		browser.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		browser.manage().window().maximize();
+		browser.navigate().to("http://localhost:8080");
+	}
+	
+	@Test
+	public void test() {
+		
+	}
+
+
+}
